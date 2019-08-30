a1e7158481b91f6f1b9ebc58a35df66ab6ca44fb
王瑞东 <ruiong.wang@tsingdata.com>
2019-08-30 15:16:58 +0800

    init

---
+++ src/main/index.js
@@ -0,0 +1,145 @@
+'use strict'
+
+import { app, BrowserWindow, Tray } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+let settingWindow
+let tray
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+const settingWinURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080/#setting/upload`
+  : `file://${__dirname}/index.html#setting/upload`
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+const toggleWindow = (bounds) => {
+  if (window.isVisible()) {
+    window.hide()
+  } else {
+    showWindow(bounds)
+  }
+}
+const showWindow = (bounds) => {
+  window.setPosition(bounds.x - 98 + 11, bounds.y, false)
+  // window.webContents.send('updateFiles')
+  window.show()
+  window.focus()
+}
+
+const createSettingWindow = () => {
+  const options = {
+    height: 450,
+    width: 800,
+    show: false,
+    frame: true,
+    center: true,
+    fullscreenable: false,
+    resizable: false,
+    title: 'PicGo',
+    vibrancy: 'ultra-dark',
+    transparent: true,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  }
+  if (process.platform === 'win32') { // 针对windows平台做出不同的配置
+    options.show = true // 创建即展示
+    options.frame = false // 创建一个frameless窗口
+    options.backgroundColor = '#3f3c37' // 背景色
+  }
+  settingWindow = new BrowserWindow(options)
+
+  settingWindow.loadURL(settingWinURL)
+
+  settingWindow.on('closed', () => {
+    settingWindow = null
+  })
+}
+
+function createTray () {
+  const menubarPic = process.platform === 'darwin' ? `${__static}/menubar.png` : `${__static}/menubar-nodarwin.png`
+  tray = new Tray(menubarPic) // 指定图片的路径
+  const contextMenu = // ...菜单
+    tray.on('right-click', () => { // 右键点击
+      window.hide() // 隐藏小窗口
+      tray.popUpContextMenu(contextMenu) // 打开菜单
+    })
+  tray.on('click', () => { // 左键点击
+    if (process.platform === 'darwin') { // 如果是macOS
+      toggleWindow() // 打开或关闭小窗口
+    } else { // 如果是windows
+      window.hide() // 隐藏小窗口
+      if (settingWindow === null) { // 如果主窗口不存在就创建一个
+        createSettingWindow()
+        settingWindow.show()
+      } else { // 如果主窗口在，就显示并激活
+        settingWindow.show()
+        settingWindow.focus()
+      }
+    }
+  })
+}
+
+app.on('ready', () => {
+  createWindow()
+  createSettingWindow()
+  if (process.platform === 'darwin' || process.platform === 'win32') {
+    createTray()
+  }
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
